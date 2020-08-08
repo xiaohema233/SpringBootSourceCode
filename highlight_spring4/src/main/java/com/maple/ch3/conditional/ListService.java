5fa0934f887416161907b1c795098a89acbf5829
xiaohema233 <33052287+xiaohema233@users.noreply.github.com>
2020-08-08 19:03:48 +0800

    3.4 条件注解@Conditional

---
+++ highlight_spring4/src/main/java/com/maple/ch3/conditional/ListService.java
@@ -0,0 +1,5 @@
+package com.maple.ch3.conditional;
+
+public interface ListService {
+	public String showListCmd();
+}
---
+++ highlight_spring4/src/main/java/com/maple/ch3/conditional/WindowsListService.java
@@ -0,0 +1,10 @@
+package com.maple.ch3.conditional;
+
+public class WindowsListService implements ListService {
+
+	@Override
+	public String showListCmd() {
+		return "dir";
+	}
+
+}
