e2b4b9d2593b22a7238ec81b5c25f6cb676d2df0
xiaohema233 <33052287+xiaohema233@users.noreply.github.com>
2020-08-08 18:56:53 +0800

    3.1 Spring Aware

---
+++ highlight_spring4/src/main/java/com/maple/ch3/aware/AwareService.java
@@ -0,0 +1,44 @@
+package com.maple.ch3.aware;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AwareService implements BeanNameAware,ResourceLoaderAware{//1
+
+	private String beanName;
+	private ResourceLoader loader;
+
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {//2
+		this.loader = resourceLoader;
+	}
+
+	@Override
+	public void setBeanName(String name) {//3
+		this.beanName = name;
+	}
+
+	public void outputResult(){
+		System.out.println("Bean的名称为：" + beanName);
+
+		// 设置aware为resource文件夹
+		Resource resource =
+				loader.getResource("test.txt");
+		try{
+
+			System.out.println("ResourceLoader加载的文件内容为: " + IOUtils.toString(resource.getInputStream()));
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
